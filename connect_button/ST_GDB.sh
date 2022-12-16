fee079554962142c3cd7897fe027ecb4c064c98a
khadievedem <khadiev.edem@gmail.com>
2022-12-17 02:19:08 +0300

    delete button init

--- connect_button/ST_GDB.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ST-LINK_gdbserver -p 61234 -l 1 -d -s -cp $CUBE_DL -m 0 -k
-# /home/khadiev/st/stm32cubeide_1.10.1/plugins/com.st.stm32cube.ide.mcu.externaltools.stlink-gdb-server.linux64_2.0.300.202203231527/tools/bin/ST-LINK_gdbserver -p 61234 -l 1 -d -s -cp /home/khadiev/st/stm32cubeide_1.10.1/plugins/com.st.stm32cube.ide.mcu.externaltools.cubeprogrammer.linux64_2.0.301.202207041506/tools/bin -m 0 -g
